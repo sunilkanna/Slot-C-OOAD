f08cce7575b25d91432caea980dc44871904bde1
sunilkanna <132731402+sunilkanna@users.noreply.github.com>
2023-08-22 08:27:25 +0530

    Add files via upload

---
+++ exp 7/cashier.java
@@ -0,0 +1,93 @@
+
+
+/**
+ * Class cashier
+ */
+public class cashier {
+
+  //
+  // Fields
+  //
+
+  private void product_name;
+  private void cost_of_product;
+  
+  //
+  // Constructors
+  //
+  public cashier () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of product_name
+   * @param newVar the new value of product_name
+   */
+  private void setProduct_name (void newVar) {
+    product_name = newVar;
+  }
+
+  /**
+   * Get the value of product_name
+   * @return the value of product_name
+   */
+  private void getProduct_name () {
+    return product_name;
+  }
+
+  /**
+   * Set the value of cost_of_product
+   * @param newVar the new value of cost_of_product
+   */
+  private void setCost_of_product (void newVar) {
+    cost_of_product = newVar;
+  }
+
+  /**
+   * Get the value of cost_of_product
+   * @return the value of cost_of_product
+   */
+  private void getCost_of_product () {
+    return cost_of_product;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void enter_amount()
+  {
+  }
+
+
+  /**
+   */
+  public void swipe_card()
+  {
+  }
+
+
+  /**
+   */
+  public void print_bill()
+  {
+  }
+
+
+  /**
+   */
+  public void delivery_product()
+  {
+  }
+
+
+}
---
+++ exp 7/central_system.java
@@ -0,0 +1,79 @@
+
+
+/**
+ * Class central_system
+ */
+public class central_system {
+
+  //
+  // Fields
+  //
+
+  private void product_name;
+  private void product_details;
+  
+  //
+  // Constructors
+  //
+  public central_system () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of product_name
+   * @param newVar the new value of product_name
+   */
+  private void setProduct_name (void newVar) {
+    product_name = newVar;
+  }
+
+  /**
+   * Get the value of product_name
+   * @return the value of product_name
+   */
+  private void getProduct_name () {
+    return product_name;
+  }
+
+  /**
+   * Set the value of product_details
+   * @param newVar the new value of product_details
+   */
+  private void setProduct_details (void newVar) {
+    product_details = newVar;
+  }
+
+  /**
+   * Get the value of product_details
+   * @return the value of product_details
+   */
+  private void getProduct_details () {
+    return product_details;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void print_bill()
+  {
+  }
+
+
+  /**
+   */
+  public void validate_card()
+  {
+  }
+
+
+}
