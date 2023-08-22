790cbdb1d4bf1b8d59c05bc55e8580521d6b9708
sunilkanna <132731402+sunilkanna@users.noreply.github.com>
2023-08-22 09:45:02 +0530

    Add files via upload

---
+++ exp 15/recepients.java
@@ -0,0 +1,57 @@
+
+import java.util.*;
+
+
+/**
+ * Class recepients
+ */
+public class recepients extends donor {
+
+  //
+  // Fields
+  //
+
+  private void request_;
+  
+  //
+  // Constructors
+  //
+  public recepients () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of request_
+   * @param newVar the new value of request_
+   */
+  private void setRequest_ (void newVar) {
+    request_ = newVar;
+  }
+
+  /**
+   * Get the value of request_
+   * @return the value of request_
+   */
+  private void getRequest_ () {
+    return request_;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void verify()
+  {
+  }
+
+
+}
