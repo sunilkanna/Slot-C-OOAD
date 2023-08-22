0881aff8633ef699330065d1c75b259bacdc81b7
sunilkanna <132731402+sunilkanna@users.noreply.github.com>
2023-08-22 08:19:16 +0530

    Add files via upload

---
+++ exp 3/reg_website.java
@@ -0,0 +1,120 @@
+
+
+/**
+ * Class reg_website
+ */
+public class reg_website {
+
+  //
+  // Fields
+  //
+
+  private void qualification;
+  private void login_details;
+  private void fee_details;
+  private void exam_dates;
+  
+  //
+  // Constructors
+  //
+  public reg_website () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of qualification
+   * @param newVar the new value of qualification
+   */
+  private void setQualification (void newVar) {
+    qualification = newVar;
+  }
+
+  /**
+   * Get the value of qualification
+   * @return the value of qualification
+   */
+  private void getQualification () {
+    return qualification;
+  }
+
+  /**
+   * Set the value of login_details
+   * @param newVar the new value of login_details
+   */
+  private void setLogin_details (void newVar) {
+    login_details = newVar;
+  }
+
+  /**
+   * Get the value of login_details
+   * @return the value of login_details
+   */
+  private void getLogin_details () {
+    return login_details;
+  }
+
+  /**
+   * Set the value of fee_details
+   * @param newVar the new value of fee_details
+   */
+  private void setFee_details (void newVar) {
+    fee_details = newVar;
+  }
+
+  /**
+   * Get the value of fee_details
+   * @return the value of fee_details
+   */
+  private void getFee_details () {
+    return fee_details;
+  }
+
+  /**
+   * Set the value of exam_dates
+   * @param newVar the new value of exam_dates
+   */
+  private void setExam_dates (void newVar) {
+    exam_dates = newVar;
+  }
+
+  /**
+   * Get the value of exam_dates
+   * @return the value of exam_dates
+   */
+  private void getExam_dates () {
+    return exam_dates;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void verification()
+  {
+  }
+
+
+  /**
+   */
+  public void processing()
+  {
+  }
+
+
+  /**
+   */
+  public void registration()
+  {
+  }
+
+
+}
---
+++ exp 3/university_databases.java
@@ -0,0 +1,72 @@
+
+
+/**
+ * Class university_databases
+ */
+public class university_databases {
+
+  //
+  // Fields
+  //
+
+  private void exam_details;
+  private void reg_details;
+  
+  //
+  // Constructors
+  //
+  public university_databases () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of exam_details
+   * @param newVar the new value of exam_details
+   */
+  private void setExam_details (void newVar) {
+    exam_details = newVar;
+  }
+
+  /**
+   * Get the value of exam_details
+   * @return the value of exam_details
+   */
+  private void getExam_details () {
+    return exam_details;
+  }
+
+  /**
+   * Set the value of reg_details
+   * @param newVar the new value of reg_details
+   */
+  private void setReg_details (void newVar) {
+    reg_details = newVar;
+  }
+
+  /**
+   * Get the value of reg_details
+   * @return the value of reg_details
+   */
+  private void getReg_details () {
+    return reg_details;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void storing_registration()
+  {
+  }
+
+
+}
